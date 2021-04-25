e3432eb974059da066ae3625b029c0f0b03030ad
f0lg0 <folgonileonardo@gmail.com>
2021-04-25 18:14:20 +0200

    new: changes to ticket cardsW

--- src/webserver/web/static/js/components/ticket_card.js
+++ src/webserver/web/static/js/components/ticket_card.js
@@ -116,7 +116,15 @@ template.innerHTML = `
             </div>
         </div>
         <div class="bottom">
-            <p>Visualizza</p>
+            <p>
+                <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4.66667 8.16667H23.3333" stroke="#FF2825" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M11.6667 12.8333V19.8333" stroke="#FF2825" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M16.3333 12.8333V19.8333" stroke="#FF2825" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M5.83333 8.16667L7 22.1667C7 22.7855 7.24583 23.379 7.68341 23.8166C8.121 24.2542 8.71449 24.5 9.33333 24.5H18.6667C19.2855 24.5 19.879 24.2542 20.3166 23.8166C20.7542 23.379 21 22.7855 21 22.1667L22.1667 8.16667" stroke="#FF2825" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M10.5 8.16667V4.66667C10.5 4.35725 10.6229 4.0605 10.8417 3.84171C11.0605 3.62292 11.3572 3.5 11.6667 3.5H16.3333C16.6428 3.5 16.9395 3.62292 17.1583 3.84171C17.3771 4.0605 17.5 4.35725 17.5 4.66667V8.16667" stroke="#FF2825" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>            
+            </p>
         </div>
     </div>
 `;
